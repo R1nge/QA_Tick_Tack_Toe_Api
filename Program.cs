0d250b4f94880b2302bbc6e7ccde1781d886cb1b
R1nge <r1ngegamedev@gmail.com>
2024-08-31 17:48:39 +0300

    feat: make a turn api

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,14 +24,57 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/randomteam", () =>
+var board = new Team[3, 3];
+
+app.MapPost("/maketurn{x:int},{y:int},{team:int}", async (HttpContext context, int x, int y, int team) =>
     {
-        var randomTeam = Enum.GetValues(typeof(Team))
-            .GetValue(Random.Shared.Next(Enum.GetValues(typeof(Team)).Length))!;
-        return randomTeam;
+        if (team <= 0 || team > Enum.GetNames(typeof(Team)).Length)
+        {
+            return context.Response.WriteAsJsonAsync(new { error = "Invalid team", error_code = "400 Bad Request" });    
+        }
+        
+        if (x < 0 || x > 2 || y < 0 || y > 2)
+        {
+            return context.Response.WriteAsJsonAsync(new { error = "Invalid coordinates", error_code = "400 Bad Request" });    
+        }
+        
+        if (board[x, y] != Team.None)
+        {
+            return context.Response.WriteAsJsonAsync(new { error = "Invalid move, cell already taken", error_code = "400 Bad Request" });    
+        }
+        
+        board[x, y] = (Team)team;
+        
+        var boardList = ConvertBoardToList(board);
+        return context.Response.WriteAsJsonAsync(boardList);
     })
-    .WithName("GetRandomTeam")
+    .WithName("MakeTurn")
+    .WithDescription("Make a turn on the board")
     .WithOpenApi();
+
+static List<List<Team>> ConvertBoardToList(Team[,] board)
+{
+    var result = new List<List<Team>>();
+    for (int i = 0; i < board.GetLength(0); i++)
+    {
+        var row = new List<Team>();
+        for (int j = 0; j < board.GetLength(1); j++)
+        {
+            row.Add(board[i, j]);
+        }
+        result.Add(row);
+    }
+    return result;
+}
+
+// app.MapGet("/randomteam", () =>
+//     {
+//         var randomTeam = Enum.GetValues(typeof(Team))
+//             .GetValue(Random.Shared.Next(Enum.GetValues(typeof(Team)).Length))!;
+//         return randomTeam;
+//     })
+//     .WithName("GetRandomTeam")
+//     .WithOpenApi();
 
 app.MapGet("/weatherforecast", () =>
     {
@@ -52,6 +98,15 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
 
+[Serializable]
+public class MakeTurnRequest
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Team { get; set; }
+}
+
+[Serializable]
 enum Team
 {
     None,
